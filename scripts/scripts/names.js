7638a95508615bb39f114b8b95ef264803a6ebec
Juliakoutsoukos <Juliakoutsoukos14@gmail.com>
2026-02-01 19:40:27 +0000

    names

---
+++ scripts/scripts/names.js
@@ -0,0 +1,12 @@
+export const names = [
+  "Julia Koutsoukos",
+  "Emma Bevans",
+  "Anna Paul",
+  "Cece Nguyen",
+  "Maddi Saber",
+  "Aiden Prince",
+  "Dani Miles",
+  "Derek Rose",
+  "Charlotte Smith",
+  "Llana Thach"
+];
